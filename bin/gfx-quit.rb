f6402d661cba451920bac2b7847dd954834c2061
Joshua Antonishen <joshua@antonishen.com>
2013-03-17 15:41:19 -0500

    added gfx card annoyance scripts

---
+++ bin/gfx-quit.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+system %Q(osascript -e 'tell application "Bartender" to quit')
+system %Q(osascript -e 'tell application "Dropbox" to quit')
+system %Q(osascript -e 'tell application "Fantastical" to quit')
